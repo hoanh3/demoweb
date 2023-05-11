c8ddd07ed323849a0cea73d369863cf1b4d10ad3
hoant <hoantch211@gmail.com>
2023-05-11 09:41:01 +0700

    init

---
+++ src/main/java/nongsan/webmvc/controller/CartController.java
@@ -0,0 +1,37 @@
+package nongsan.webmvc.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.http.HttpServlet;
+
+
+/**
+ * Servlet implementation class CartController
+ */
+
+public class CartController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view/client/cart.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		super.doPost(request, response);
+	}
+
+}
